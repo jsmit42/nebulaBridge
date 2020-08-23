b351213360764d2d662df065d5f218af52090432
Jacob <jsmit4266@gmail.com>
2020-08-23 14:26:34 -0700

    Added colors.h

---
+++ colors.h
@@ -0,0 +1,44 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define RESET "\e[0m"
+
+//text colors
+#define BLACK        "\e[30m"
+#define RED          "\e[31m"
+#define GREEN        "\e[32m"
+#define YELLOW       "\e[33m"
+#define BLUE         "\e[34m"
+#define MAGENTA      "\e[35m"
+#define CYAN         "\e[36m"
+#define WHITE        "\e[37m"
+#define GREY               "\e[90m"
+#define GRAY               "\e[90m"
+#define BRIGHT_RED          "\e[91m"
+#define BRIGHT_GREEN        "\e[92m"
+#define BRIGHT_YELLOW       "\e[93m"
+#define BRIGHT_BLUE         "\e[94m"
+#define BRIGHT_MAGENTA      "\e[95m"
+#define BRIGHT_CYAN         "\e[96m"
+#define BRIGHT_WHITE        "\e[97m"
+
+// background colors
+#define BACK_BLACK         "\E[40M"
+#define BACK_RED           "\E[41M"
+#define BACK_GREEN         "\E[42M"
+#define BACK_YELLOW        "\E[43M"
+#define BACK_BLUE          "\E[44M"
+#define BACK_MAGENTA       "\E[45M"
+#define BACK_CYAN          "\E[46M"
+#define BACK_WHITE         "\E[47M"
+#define BACK_GREY          "\E[100M"
+#define BACK_GRAY          "\E[100M"
+#define BACK_BRIGHT_RED     "\E[101M"
+#define BACK_BRIGHT_GREEN   "\E[102M"
+#define BACK_BRIGHT_YELLOW  "\E[103M"
+#define BACK_BRIGHT_BLUE    "\E[104M"
+#define BACK_BRIGHT_MAGENTA "\E[105M"
+#define BACK_BRIGHT_CYAN    "\E[106M"
+#define BACK_BRIGHT_WHITE   "\E[107M"
+
+#endif
